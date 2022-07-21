13763ebe5f8ae9d6a4263f6a3955e8f164a2877f
skylervautier <skylervautier@quadax.com>
2022-07-21 14:32:38 -0400

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App(): JSX.Element {
+	return (
+		<div className="maw-[1200px] p-[1rem] mx-[auto] min-h[100vh] d-[flex] fld-[column]">
+			<h1>Hello World!</h1>
+		</div>
+	);
+}
+
+export default App;
